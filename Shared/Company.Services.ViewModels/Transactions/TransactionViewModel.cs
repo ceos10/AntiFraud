0f55147156309926a0b0d92fbff36dc6fba00142
ceos10 <carlos.ocrospoma@globant.com>
2025-04-30 23:39:57 -0500

    Adding status to model

--- Shared/Company.Services.ViewModels/Transactions/TransactionViewModel.cs
+++ Shared/Company.Services.ViewModels/Transactions/TransactionViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace Company.Services.ViewModels.Transactions;
+﻿using Company.Services.ViewModels.Transactions.Enums;
+
+namespace Company.Services.ViewModels.Transactions;
 
 public record TransactionViewModel
 {
     public Guid TransactionExternalId { get; set; }
     public DateTime CreatedAt { get; set; }
+    public TransactionStatusViewModel Status { get; set; } //Added to validate status in GetTransactionById
 }
